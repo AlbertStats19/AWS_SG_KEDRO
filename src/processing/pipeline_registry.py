de029c8f1b7589bb3bd7622908802de4fd91aef7
AlbertStats19 <ycaperam@unal.edu.co>
2025-09-26 19:25:58 -0500

    kedro project2

--- src/processing/pipeline_registry.py
+++ src/processing/pipeline_registry.py
@@ -3,17 +3,15 @@
 
 from __future__ import annotations
 from kedro.pipeline import Pipeline
-from processing.pipelines.backtesting import create_pipeline as backtesting_pipeline
 
+# 👉 Reexporta desde TU paquete real (no desde processing)
+from data_bbog_integration_fabrica_personas.pipelines.backtesting import (
+    create_pipeline as backtesting_pipeline,
+)
 
 def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
-
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
     pipelines = {
-        "backtesting": backtesting_pipeline()
+        "backtesting": backtesting_pipeline(),
     }
     pipelines["__default__"] = pipelines["backtesting"]
     return pipelines
